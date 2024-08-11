114292b67ea427f5370648cf9cf39263f32e0af0
rMi99 <ramisha.rightmo@gmail.com>
2024-08-11 12:38:18 +0530

    Add Class

---
+++ Hi.java
@@ -0,0 +1,11 @@
+public class Hi {
+
+    public static void main(String[] args){
+        cal(10);
+    }
+    public static void cal(int num){
+        int id = 10;
+        int sum = num + id;
+        System.out.println(sum);
+    }
+}
